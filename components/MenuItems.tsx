ccc8fdfbd9d3a34c895eebfa9dff745aecf45d00
BartlomiejSadza <bartlomiej.sadza@icloud.com>
2025-01-13 23:24:46 +0100

    inne dymy - tym razem SectionList

--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,56 +1,72 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SectionList } from "react-native";
 import Colors from "constants/Colors";
 
 const menuItemsToDisplay = [
-	{ name: "Hummus", price: "$5.00", id: "1A" },
-	{ name: "Moutabal", price: "$5.00", id: "2B" },
-	{ name: "Falafel", price: "$7.50", id: "3C" },
-	{ name: "Marinated Olives", price: "$5.00", id: "4D" },
-	{ name: "Kofta", price: "$5.00", id: "5E" },
-	{ name: "Eggplant Salad", price: "$8.50", id: "6F" },
-	{ name: "Lentil Burger", price: "$10.00", id: "7G" },
-	{ name: "Smoked Salmon", price: "$14.00", id: "8H" },
-	{ name: "Kofta Burger", price: "$11.00", id: "9I" },
-	{ name: "Turkish Kebab", price: "$15.50", id: "10J" },
-	{ name: "Fries", price: "$3.00", id: "11K" },
-	{ name: "Buttered Rice", price: "$3.00", id: "12L" },
-	{ name: "Bread Sticks", price: "$3.00", id: "13M" },
-	{ name: "Pita Pocket", price: "$3.00", id: "14N" },
-	{ name: "Lentil Soup", price: "$3.75", id: "15O" },
-	{ name: "Greek Salad", price: "$6.00", id: "16Q" },
-	{ name: "Rice Pilaf", price: "$4.00", id: "17R" },
-	{ name: "Baklava", price: "$3.00", id: "18S" },
-	{ name: "Tartufo", price: "$3.00", id: "19T" },
-	{ name: "Tiramisu", price: "$5.00", id: "20U" },
-	{ name: "Panna Cotta", price: "$5.00", id: "21V" },
+	{
+		title: "Appetizers",
+		data: [
+			"Hummus",
+			"Moutabal",
+			"Falafel",
+			"Marinated Olives",
+			"Kofta",
+			"Eggplant Salad",
+		],
+	},
+	{
+		title: "Main Dishes",
+		data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+	},
+	{
+		title: "Sides",
+		data: [
+			"Fries",
+			"Buttered Rice",
+			"Bread Sticks",
+			"Pita Pocket",
+			"Lentil Soup",
+			"Greek Salad",
+			"Rice Pilaf",
+		],
+	},
+	{
+		title: "Desserts",
+		data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+	},
 ];
 
-interface Dish {
-	name: string;
-	price: string;
-	id: string;
+interface MenuSection {
+	title: string;
+	data: string[];
 }
 
-const Item = (item: Dish) => {
+const Item = ({ item }: { item: string }) => {
 	return (
 		<View style={styles.list}>
-			<Text style={styles.list_item}>{item.name}</Text>
-			<Text style={styles.list_item}>{item.price}</Text>
+			<Text style={styles.list_item}>{item}</Text>
 		</View>
 	);
 };
 
 export default function MenuItems() {
-	const renderItem = ({ item }: { item: Dish }) => <Item {...item} />;
+	const renderItem = ({ item }: { item: string }) => <Item item={item} />;
+
+	const renderSectionHeader = ({
+		section: { title },
+	}: {
+		section: MenuSection;
+	}) => <Text style={styles.title}>{title}</Text>;
+
+	const Separator = () => <View style={styles.separator} />;
 
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Menu List</Text>
-			<FlatList
-				data={menuItemsToDisplay}
-				keyExtractor={(item) => item.id}
+			<SectionList
+				keyExtractor={(item) => item}
+				sections={menuItemsToDisplay}
 				renderItem={renderItem}
-				ItemSeparatorComponent={() => <View style={styles.separator} />}
+				renderSectionHeader={renderSectionHeader}
+				ItemSeparatorComponent={Separator}
 			/>
 		</View>
 	);
